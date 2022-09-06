0004f4861a0ca92945d3b8c19162a5bf8c66133d
unknown <masukankatasandi2292@gmail.com>
2022-09-06 20:54:42 +0700

    Second

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,14 +3,14 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
 
 const Footer = () => {
-  const icon = <FontAwesomeIcon icon={faGithub} />;
+  // const icon = <FontAwesomeIcon icon={faGithub} />;
 
   return (
     <div className="text-center border-t border-slate-300 p-4 text-slate-400">
-      <div>Designed and built by Verdi</div>
-      <Link href="https://github.com">
+      <div>Built by Verdi</div>
+      {/* <Link href="https://github.com">
         <a className="text-3xl text-slate-300">{icon}</a>
-      </Link>
+      </Link> */}
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
